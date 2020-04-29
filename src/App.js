987b6adcb852b74f7b9d82d23f92ab7bd945b14c
Pavel Parvez <pavel.cse26@gmail.com>
2020-04-29 20:54:42 +0600

    Update our menu-item to navigate withRouter High Order Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomeComponent from "./pages/homepage/HomeComponent";
 
+const HatsPage = () => (
+    <h1>Hello Home</h1>
+);
+
 function App() {
   return (
     <div>
-        <HomeComponent />
+        <Switch>
+            <Route exact path='/' component={HomeComponent} />
+            <Route path='/hats' component={HatsPage} />
+        </Switch>
     </div>
   );
 }
